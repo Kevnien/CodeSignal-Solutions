6c75e4f90b1afe88d5739b65fe01e97db41944cc
Kevin Nguyen <kevinn858@gmail.com>
2019-02-08 20:27:39 -0800

    added description for areEquallyStrong

--- Intro/Island of Knowledge/areEquallyStrong.java
+++ Intro/Island of Knowledge/areEquallyStrong.java
@@ -1,3 +1,8 @@
+// Call two arms equally strong if the heaviest weights they each are able to lift are equal.
+
+// Call two people equally strong if their strongest arms are equally strong (the strongest arm can be both the right and the left), and so are their weakest arms.
+
+// Given your and your friend's arms' lifting capabilities find out if you two are equally strong.
 class areEquallyStrong{
     public static boolean areEquallyStrong(int yourLeft, int yourRight, int friendsLeft, int friendsRight) {
         //I realize that I can reduce run time by combining comparisons into blocks
