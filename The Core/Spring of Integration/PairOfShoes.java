ce4ec03828c515cb01d1330f5ee55c1e7c3a86a8
Kevin Nguyen <kevinn858@gmail.com>
2019-04-17 21:38:58 -0700

    solved PairOfShoes

--- The Core/Spring of Integration/PairOfShoes.java
+++ The Core/Spring of Integration/PairOfShoes.java
@@ -7,7 +7,7 @@
 import java.util.HashMap;
 class PairOfShoes{
     public static boolean pairOfShoes(int[][] shoes){
-        if(shoes.lenth%2 == 1){
+        if(shoes.length%2 == 1){
             return false;
         }
         int count = 0;
@@ -15,23 +15,36 @@ class PairOfShoes{
         for(int i=0; i<shoes.length; i++){
             Integer[] value = map.get(shoes[i][1]);
             if(value == null){
-                Integer[] newValue = {
-                    shoes[i][0],
-                    1
-                };
+                Integer[] newValue = {shoes[i][0], 1};
                 map.put(shoes[i][1], newValue);
                 continue;
             }
             if(shoes[i][0] != value[0]){
                 count += 2;
-                Integer amount = value[1] - 1;
-                if(amount == 0){
+                value[1]--;
+                if(value[1] == 0){
                     map.remove(shoes[i][1]);
+                }else{
+                    map.put(shoes[i][1], value);
                 }
-                continue;
             }else{
-                
+                value[1]++;
+                map.put(shoes[i][1], value);
             }
         }
+        if(count == shoes.length){
+            return true;
+        }
+        return false;
+    }
+
+    public static void main(String[] args){
+        int[][] shoes = {
+            {0,21},
+            {1,23},
+            {1,21},
+            {1,23}
+        };
+        System.out.println(pairOfShoes(shoes));
     }
 }
